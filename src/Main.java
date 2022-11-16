0090c12a3bf7b6fda11bc3e4779cb92a7bbee747
Дамир <damir.obuhov@mail.ru>
2022-11-16 17:10:06 +0400

    Да так код  выглядит лучше!

--- src/Main.java
+++ src/Main.java
@@ -1,76 +1,70 @@
 import java.util.Arrays;
 
 
+//Задание 1
 
-
-
-        //Задание 1public static void task1()
-        public class Main {
-        int[]arr=generateRandomArray();
+public class Main {
+    public static void task1() {
+        int[] arr = generateRandomArray();
         System.out.println(Arrays.toString(arr));
-        int sum=0;
-        for(int element:arr){
+        int sum = 0;
+        for (int element : arr) {
 
         }
-       // System.out.println("Сумма трат за месяц составит "+sum+" рублей\n");
 
-                //Задание 2
-                public static void задание2() {
-                        int[] arr = generateRandomArray();
-                        System.out.println(Arrays.toString(arr));
 
-                        int min = arr[0];
-                        for (int i = 1; i < arr.length - 1; i++) {
-                                if (min > arr[i]) {
-                                        min = arr[i];
-                                }
-                        }
-                        System.out.println("Минимальная сумма трат за день составила " + min + " рублей");
+        //Задание 2
+        public static void task2(){
+            int[] arr=generateRandomArray();
+            System.out.println(Arrays.toString(arr));
 
-                        int max = arr[0];
-                        for (int i = 1; i < arr.length - 1; i++) {
-                                if (max < arr[i]) {
-                                        max = arr[i];
-                                }
-                        }
-                        System.out.println("Максимальная сумма трат за день составила " + max + " рублей\n");
+            int min = arr[0];
+            for (int i = 1; i < arr.length - 1; i++) {
+                if (min > arr[i]) {
+                    min = arr[i];
                 }
-                //Задание 3
-                public static void задание3() {
-                        int[] arr = generateRandomArray();
-                        System.out.println(Arrays.toString(arr));
+            }
+            System.out.println("Минимальная сумма трат за день составила " + min + " рублей");
 
-                        int sum = 0;
-                        for (int element : arr) {
-                                sum += element;
-                        }
-                        float average = sum / (float) arr.length;
-                        System.out.println("Средняя сумма трат за месяц составила " + average + " рублей\n");
+            int max = arr[0];
+            for (int i = 1; i < arr.length - 1; i++) {
+                if (max < arr[i]) {
+                    max = arr[i];
                 }
+            }
+            System.out.println("Максимальная сумма трат за день составила " + max + " рублей\n");
+        }
+        //Задание 3
+        public static void task3 () {
+            int[] arr = generateRandomArray();
+            System.out.println(Arrays.toString(arr));
+
+            int sum = 0;
+            for (int element : arr) {
+                sum += element;
+            }
+            float average = sum / (float) arr.length;
+            System.out.println("Средняя сумма трат за месяц составила " + average + " рублей\n");
+        }
 
 
+        //Задание 4
 
+        public static void task4 () {
 
+            char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+            for (int i = reverseFullName.length - 1; i >= 0; i--) {
+                System.out.print(reverseFullName[i]);
+            }
+        }
 
-
-
-
-             //Задание 4
-
-                public static void задание4(){
-
-        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-        for (int i = reverseFullName.length - 1; i >= 0; i--) {
-            System.out.print(reverseFullName[i]);
+        public static int[] generateRandomArray () {
+            java.util.Random random = new java.util.Random();
+            int[] arr = new int[30];
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = random.nextInt(100_000) + 100_000;
+            }
+            return arr;
         }
     }
-
-                public static int[] generateRandomArray() {
-                        java.util.Random random = new java.util.Random();
-                        int[] arr = new int[30];
-                        for (int i = 0; i < arr.length; i++) {
-                                arr[i] = random.nextInt(100_000) + 100_000;
-                        }
-                        return arr;
-                }
-        }
+}
